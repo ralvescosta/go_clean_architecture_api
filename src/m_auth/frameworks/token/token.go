22112ea2e9179af344e64131a06f1d0da58bd683
rafael <rafael.rac.mg@gmail.com>
2020-10-06 20:38:22 -0300

    feat: auth module

---
+++ src/m_auth/frameworks/token/token.go
@@ -0,0 +1,36 @@
+package authframeworkstoken
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// IToken ...
+type IToken interface {
+	VerifyToken(t string) (*jwt.Token, error)
+}
+
+type token struct{}
+
+// VerifyToken ...
+func (*token) VerifyToken(t string) (*jwt.Token, error) {
+
+	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+// TokenConstructor ...
+func TokenConstructor() IToken {
+	return &token{}
+}
