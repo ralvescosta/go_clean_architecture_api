f450f3dd32564e383858677437061f8d807563cb
rafael <rafael.rac.mg@gmail.com>
2020-10-12 08:55:00 -0300

    test: signin_module application usecase

--- src/signin_module/application/usecases/signin_usecase_test.go
+++ src/signin_module/application/usecases/signin_usecase_test.go
@@ -1,8 +1,7 @@
 package signinapplicationusecases
 
 import (
-	core "gomux_gorm/src/core_module/bussiness/errors"
-
+	"errors"
 	tables "gomux_gorm/src/core_module/frameworks/database/table_models"
 	bussiness "gomux_gorm/src/signin_module/bussiness/entities"
 	entities "gomux_gorm/src/signin_module/bussiness/entities"
@@ -15,54 +14,103 @@ import (
 /*
 * User Repository Spy
  */
-type userRepositoryStructSpy struct{}
+type userRepositoryStructSpy struct {
+	res *tables.Users
+}
 
-func (*userRepositoryStructSpy) Create(registerUser *entities.RegisterUsersEntity) *tables.Users {
-	return &tables.Users{}
+func (u *userRepositoryStructSpy) Create(registerUser *entities.RegisterUsersEntity) *tables.Users {
+	return u.res
 }
-func (*userRepositoryStructSpy) FindByEmail(email string) *tables.Users {
-	return &tables.Users{}
+func (u *userRepositoryStructSpy) FindByEmail(email string) *tables.Users {
+	return u.res
 }
-func UserRepositorySpy() repositories.IUserRepository {
-	return &userRepositoryStructSpy{}
+func UserRepositorySpy(userRepoRes *tables.Users) repositories.IUserRepository {
+	return &userRepositoryStructSpy{res: userRepoRes}
 }
+
+/**/
 
 /*
 * Permission Repository Spy
  */
-type userPermissionsRepositoryStructSpy struct{}
+type userPermissionsRepositoryStructSpy struct {
+	res *tables.UsersPermissions
+}
 
-func (*userPermissionsRepositoryStructSpy) Create(user *tables.Users, permissionID int64, permission string) *tables.UsersPermissions {
-	return &tables.UsersPermissions{}
+func (u *userPermissionsRepositoryStructSpy) Create(user *tables.Users, permissionID int64, permission string) *tables.UsersPermissions {
+	return u.res
 }
-func UserPermissionsRepositorySpy() repositories.IUsersPermissionsRepository {
-	return &userPermissionsRepositoryStructSpy{}
+func UserPermissionsRepositorySpy(userPermissionRepoRes *tables.UsersPermissions) repositories.IUsersPermissionsRepository {
+	return &userPermissionsRepositoryStructSpy{res: userPermissionRepoRes}
 }
+
+/**/
 
 /*
 * Crypto Spy
  */
-type cryptoStructSpy struct{}
-
-func (*cryptoStructSpy) HashPassword(password string) (string, error) {
-	return "", nil
-}
-func CryptoSpy() crypto.IHasher {
-	return &cryptoStructSpy{}
+type cryptoStructSpy struct {
+	res string
+	err error
 }
 
-func TestSigninUsecase(t *testing.T) {
-	userRepositorySpy := UserRepositorySpy()
-	userPermissionsRepositorySpy := UserPermissionsRepositorySpy()
-	cryptoSpy := CryptoSpy()
+func (c *cryptoStructSpy) HashPassword(password string) (string, error) {
+	return c.res, c.err
+}
+func CryptoSpy(cryptoRes string, cryptoErr error) crypto.IHasher {
+	return &cryptoStructSpy{res: cryptoRes, err: cryptoErr}
+}
+
+/**/
+
+/*
+* MAKE SUT
+ */
+func makeSut(userRepoRes *tables.Users, userPermissionRepoRes *tables.UsersPermissions, cryptoRes string, cryptoErr error) ISigninUsecase {
+	userRepositorySpy := UserRepositorySpy(userRepoRes)
+	userPermissionsRepositorySpy := UserPermissionsRepositorySpy(userPermissionRepoRes)
+	cryptoSpy := CryptoSpy(cryptoRes, cryptoErr)
 
 	sut := SigninUsecase(&userRepositorySpy, &userPermissionsRepositorySpy, &cryptoSpy)
 
+	return sut
+}
+
+/**/
+
+func TestSigninUsecase(t *testing.T) {
+	sut := makeSut(&tables.Users{}, &tables.UsersPermissions{}, "", nil)
+
 	result := sut.SigninUsecase(&bussiness.RegisterUsersEntity{})
 
-	switch result.(type) {
-	case *core.UnauthorizedError:
-		t.Errorf("Sum was incorrect")
-		break
+	if result != nil {
+		t.Errorf("SigninUsecase()")
+	}
+}
+
+func TestShouldReturnConflictErrorIfUserEmailAlreadyExist(t *testing.T) {
+	sut := makeSut(
+		&tables.Users{
+			ID: 1,
+		},
+		&tables.UsersPermissions{},
+		"",
+		nil,
+	)
+
+	result := sut.SigninUsecase(&bussiness.RegisterUsersEntity{})
+
+	if result.Error() != "Something went wrong with the  request. Server returned 409 status." {
+		t.Errorf("SigninUsecase, check if user already exist, Unexpected Response")
+	}
+}
+
+func TestShouldReturnUnauthorizedErrorIfSomeErrorOccurInCreateHashPassword(t *testing.T) {
+	sut := makeSut(&tables.Users{}, &tables.UsersPermissions{}, "", errors.New("some error"))
+
+	result := sut.SigninUsecase(&bussiness.RegisterUsersEntity{})
+
+	if result.Error() != "Something went wrong with the  request. Server returned 401 status." {
+		t.Errorf("SigninUsecase, check if created hash password, Unexpected Response")
 	}
 }
